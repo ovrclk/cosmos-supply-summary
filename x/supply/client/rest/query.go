af282e26d2c8e92a0b06d1caa9aefcc21de6df39
akhilkumarpilli <akhilkumar7947@gmail.com>
2020-11-11 17:09:08 +0530

    Address  PR comments

--- x/supply/client/rest/query.go
+++ x/supply/client/rest/query.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 
@@ -14,11 +15,6 @@ import (
 	"github.com/ovrclk/cosmos-supply-summary/x/supply/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
-var (
-	decimals float64 = 6
-	denom            = "uakt"
 )
 
 // RegisterRoutes registers all query routes
@@ -36,32 +32,49 @@ func circulatingSupplyHandler(ctx context.CLIContext) http.HandlerFunc {
 				return
 			}
 			rest.PostProcessResponse(w, ctx, res)
-		} else {
-			res, err := query.NewClient(ctx, types.ModuleName).CirculatingSupply()
-			if err != nil {
-				rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-				return
-			}
-			metric := sdk.Coins{}
-			if q == "total" {
-				metric = res.Total
-			} else if q == "circulating" {
-				metric = res.Circulating
-			} else {
-				rest.WriteErrorResponse(w, http.StatusBadRequest, "wrong q value")
-				return
-			}
-			supply := sdk.Coin{}
-			for _, coin := range metric {
-				if coin.Denom == denom {
-					supply = coin
-				}
-			}
-			if supply.Denom == "" {
-				rest.WriteErrorResponse(w, http.StatusBadRequest, "no supply found with default denom")
-				return
-			}
-			fmt.Fprintf(w, "%.6f", float64(supply.Amount.Int64())/math.Pow(10, decimals))
+			return
 		}
+		denom := r.URL.Query().Get("denom")
+		decString := r.URL.Query().Get("decimal")
+		if len(denom) == 0 {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "`denom` value is required")
+			return
+		}
+		if len(decString) == 0 {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "`decimal` value is required")
+			return
+		}
+
+		decimals, err := strconv.ParseFloat(decString, 64)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		res, err := query.NewClient(ctx, types.ModuleName).CirculatingSupply()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		metric := sdk.Coins{}
+		if q == "total" {
+			metric = res.Total
+		} else if q == "circulating" {
+			metric = res.Circulating
+		} else {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "wrong query value, allowed values are `total`, `circulating`")
+			return
+		}
+		supply := sdk.Coin{}
+		for _, coin := range metric {
+			if coin.Denom == denom {
+				supply = coin
+			}
+		}
+		if supply.Denom == "" {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "supply not found with given denom")
+			return
+		}
+		fmt.Fprintf(w, "%.6f", float64(supply.Amount.Int64())/math.Pow(10, decimals))
 	}
 }
